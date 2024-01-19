ad1651397c1b5382f6405dfaa3a69816ddd60335
Furkan <60063034+furqanulaalam@users.noreply.github.com>
2024-01-19 13:04:44 +0530

    Update authenticateToken.js

--- backend/middleware/authenticateToken.js
+++ backend/middleware/authenticateToken.js
@@ -7,7 +7,7 @@ import Token from "../models/token.js";
 async function authenticateToken(req, res, next) {
   const authHeader = req.headers["authorization"];
   const token = authHeader && authHeader.split(" ")[1];
-  if (token === null) {
+  if (!token) {
     return res.sendStatus(401);
   } else {
     const existing = await Token.findOne({ token: token });
